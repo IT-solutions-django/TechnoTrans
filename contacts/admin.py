d3931e50ada9b7453db7a0bebb8ff5caab3da875
Пак Сергей <sergegnabri2005@gmail.com>
2025-02-14 14:09:08 +0700

    Add Privacy Policy page

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import (
     CompanyInfo,
+    PrivacyPolicy, 
+    PrivacyPolicyParagraph,
 )
 
 
 @admin.register(CompanyInfo)
 class CompanyInfoAdmin(admin.ModelAdmin): 
     list_display = ['__str__']
+    
+
+class PrivacyPolicyParagraphInline(admin.TabularInline):
+    model = PrivacyPolicyParagraph
+    extra = 1  
+    verbose_name = "Абзац"
+    verbose_name_plural = "Абзацы"
+
+
+@admin.register(PrivacyPolicy)
+class PrivacyPolicyAdmin(admin.ModelAdmin): 
+    list_display = ['__str__']
+    inlines = [PrivacyPolicyParagraphInline]
