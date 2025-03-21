ab0d91a7fdea946a753728c29399743e942211e6
Пак Сергей <sergegnabri2005@gmail.com>
2025-03-21 11:41:35 +0700

    Change server IP

--- home/services.py
+++ home/services.py
@@ -3,7 +3,7 @@ import requests
 
 def send_email(subject: str, content: str) -> None: 
         recipient = 'technotransllc@mail.ru'
-        ip = '83.222.8.148'
+        ip = '5.35.84.189'
         url = 'https://sendemail.space/send-email/'
         data = {
             'recipient': recipient, 
