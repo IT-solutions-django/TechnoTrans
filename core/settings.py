9e5c69142f870c58f3804b8eabfe653322c0b2cd
Пак Сергей <sergegnabri2005@gmail.com>
2025-03-24 15:27:32 +0700

    Make sone generators fields nullable

--- core/settings.py
+++ core/settings.py
@@ -28,12 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mb7o)a)-65pp_viswiniqb$l^*^&kirv%962yv*fz*or^cilyt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://*.83.222.8.148', 
     'https://*ttrans.pro',
     'https://www.ttrans.pro',
 ]
