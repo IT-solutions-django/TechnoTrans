9f46c9e8df0e3f842c44e85485d1d6bed346812d
Пак Сергей <sergegnabri2005@gmail.com>
2025-04-18 17:26:37 +0700

    Update Sitemap.xml

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -2,6 +2,7 @@ from django.contrib.sitemaps import Sitemap
 from django.shortcuts import reverse
 from containers.models import Container
 from generators.models import Generator 
+from universal_containers.models import UniversalContainer
 
 
 class Site: 
@@ -33,11 +34,13 @@ class StaticViewSitemap(BaseSitemap):
 
     def items(self):
         return [ 
-            'containers:catalog', 
+            'containers:catalog',
+            'universal_containers:catalog', 
             'generators:catalog',
             'containers:rent',
             'generators:rent',
             'contacts:about_company', 
+            'contacts:cargo_transportation', 
             'contacts:documentation',
             'contacts:contacts',
         ]  
@@ -58,3 +61,10 @@ class GeneratorViewSitemap(BaseSitemap):
 
     def items(self): 
         return Generator.objects.all()
+    
+
+class UnversalContainerViewSitemap(BaseSitemap): 
+    priority = 0.7 
+
+    def items(self): 
+        return UniversalContainer.objects.all()
